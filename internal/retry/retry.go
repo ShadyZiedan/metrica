f3be4234c98c52288b790f106aa848080c00097b
Шади <s.ziedan@smartapp.su>
2024-02-21 17:16:00 +0300

    Iteration 13: fixing for tests

--- internal/retry/retry.go
+++ internal/retry/retry.go
@@ -7,6 +7,13 @@ import (
 
 func WithBackoff(ctx context.Context, maxRetries int, isRetryable func(error) bool, callback func() error) error {
 	var err error
+	err = callback()
+	if err == nil {
+		return nil
+	}
+	if maxRetries <= 0 || (maxRetries > 0 && !isRetryable(err)) {
+		return err
+	}
 	for i := 1; i <= maxRetries; i++ {
 		select {
 		case <-ctx.Done():
